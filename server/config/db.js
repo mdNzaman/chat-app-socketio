3a45d9224d6fd0fd01a23182ead83d0b2fc7a092
mdNzaman <nooruzzaman18@gmail.com>
2023-06-15 09:07:55 +0000

    WIP: server

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const dbCon = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345",
+    database: "chat_app"
+})
+
+dbCon.connect((err)=>{
+    if(err) console.log(err);
+    else console.log("Database connected successfully!!..");
+})
+
+module.exports = dbCon;
---
+++ server/model/registerUserModel.js
@@ -0,0 +1,34 @@
+const dbCon = require("../config/db");
+// const {registerUser} = require("../model")
+
+exports.registerUserController = (async (req,res)=>{
+    try{
+    // async (req,res)=>{
+    const body = req.body;
+    const user_name = body.user_name;
+    const room_id = body.room_id;
+    const response =  await registerUser(user_name,room_id);
+    
+    res.status(200).json({
+        success:1,
+        user_name:user_name,
+        room_id:room_id
+    })
+}catch(err){
+    console.log(err)
+}
+// }
+})
+
+
+registerUser = ((userName,roomId)=>{
+    return new Promise ((resolve,reject)=>{
+        dbCon.query(`INSERT INTO join_room (user_name,room)
+        VALUES (?,?)`,[userName,roomId],(err,result)=>{
+            if(err){
+                reject(err);
+            }
+            resolve(result);
+        })
+    })
+})
