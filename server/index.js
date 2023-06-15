c032cd7e46ca37eba7a3ba1d0a0e82ade93f1e5d
mdNzaman <nooruzzaman18@gmail.com>
2023-06-15 09:05:54 +0000

    WIP: server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const {Server} = require("socket.io")
+const {Server} = require("socket.io");
+const {registerUser,registerUserController} = require("./model/registerUserModel");
 
+app.use(express.json()); // due to this i was not getting my body-content
 app.use(cors());
 
 const server = http.createServer(app);
@@ -14,8 +16,45 @@ const io = new Server(server, {
         methods:["GET","POST"],
     },
 })
+// function data(a){
+//     a = 10;
+//     return a;
+    // json({success:1,
+    //     message:"api working"})
+// }
+
+app.get("/",async (req,res)=>{
+    console.log("apis working");
+    // let a;
+    // const dat = await data(a);
+    res.status(200).json({
+     // data: dat
+    // success:1,
+    // message:"api working"
+    })   
+})
+
+app.post("/register-user", registerUserController
+// async (req,res)=>{
+//     console.log("bug..");
+//     console.log(req.user_name);
+//     console.log(req.body);
+//     const body = req.body;
+//     console.log(body);
+//     const user_name = body.user_name;
+//     const room_id = body.room_id;
+//     const response =  await registerUser(user_name,room_id);
+//     res.status(200).json({
+//         success:1,
+//         message:"api working"
+//     })
+// }
+)
 
 io.on("connection", (socket)=>{
+
+    // socket.removeAllListeners();
+    
     console.log(`User Connected: ${socket.id}`);
 
     socket.on("join_room", (data)=>{   // data-->id
