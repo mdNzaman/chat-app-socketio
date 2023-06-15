030f0a2cbc3df98e476dbba3f1d6d078cb9a84cd
mdNzaman <nooruzzaman18@gmail.com>
2023-06-15 10:11:07 +0000

    WIP: client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
 import io from "socket.io-client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Chat from './chat'
 
 const socket = io.connect("http://localhost:3001");
@@ -10,13 +10,42 @@ function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
+  useEffect(() => {
+    console.log("check");
+    socket.on("receive_message", (data) => {
+      // data = "";
 
-  const joinRoom = () => {
+      console.log("data ", data);
+      // setMessageList((list) => [...list, data]);
+      // {console.log("2"+setMessageList);}
+    });
+  }, []);
+
+  const joinRoom = async () => {
     if (username !== "" && room !== "") {
+
+      // getChatList(username,room);
+
+      const response = await fetch("http://localhost:3001/" + "register-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_name:username,
+          room_id:room
+        }),
+        // body: JSON.stringify(credentials),
+      }).then((data) => data.json());
+
+      console.log(response);
+      // console.log(data);
 
       socket.emit("join_room", room);
 
       setShowChat(true);
+    }else{
+      console.log("something missing");
     }
   };
 
--- client/src/chat.js
+++ client/src/chat.js
@@ -1,99 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import ScrollToBottom from 'react-scroll-to-bottom'
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 // import "./App.css";
 
-export default function Chat({socket, name, room}){
+export default function Chat({ socket, name, room }) {
+  // {console.log(socket,name,room)}
+  let myStyle = {
+    backgroundColor: "#43a047",
+  };
 
-    // {console.log(socket,name,room)}
-    let myStyle = {
-        backgroundColor:'#43a047'
-      }
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  // const [renderChat, setRenderChat] = useState(false);
 
-const [currentMessage,setCurrentMessage] = useState("");
-const [messageList,setMessageList] = useState([]);
-// const [renderChat, setRenderChat] = useState(false);
+  const sendMessgae = async () => {
+    if (currentMessage !== "") {
 
-const sendMessgae = async ()=>{
-    if(currentMessage!==""){
-        const messageData = {
-            room: room,
-            author: name,
-            message: currentMessage,
-            time: new Date(Date.now()).getHours()+
-             ":" + new Date(Date.now()).getMinutes(),
-        };
+      const messageData = {
+        room: room,
+        author: name,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
 
-        await socket.emit("send_message",messageData);
-        setMessageList((list)=>
-            // console.log(list)
-            [...list, messageData])
-        setCurrentMessage("");
-// {console.log("1"+setMessageList);}
-        // setRenderChat(true);
+      await socket.emit("send_message", messageData);
+      setMessageList((list) =>
+        // console.log(list)
+        [...list, messageData]
+      );
+      setCurrentMessage("");
+      // {console.log("1"+setMessageList);}
+      // setRenderChat(true);
     }
-};
+  };
 
-useEffect(()=>{
-    socket.on("receive_message",(data)=>{
-        // data = "";
-       
-        console.log("data "+ data);
-        setMessageList((list)=>
-            [...list,data]
-        );
-        // {console.log("2"+setMessageList);}
+  useEffect(() => {
+    console.log("check");
+    socket.on("receive_message", (data) => {
+      // data = "";
+
+      console.log("data ", data);
+      setMessageList((list) => [...list, data]);
+      // {console.log("2"+setMessageList);}
     });
-}, [socket]);
+  }, []);
 
   return (
-    <div className='chat-window'>
-        <div className='chat-header'>
-            <p>Live Chat</p>
-        </div>
-        {/* {renderChat ? ( */}
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      {/* {renderChat ? ( */}
 
-        <div className='chat-body'>
-            
+      <div className="chat-body">
         {/* <div > */}
 
-            {console.log(messageList)}  
-            {console.log("bug")}
-            <ScrollToBottom className="message-container">
+        {console.log(messageList)}
+        {console.log("bug")}
 
-            {messageList.map((messageContent)=>{
-                // return <h1>{messageContent.message}</h1>
-                return (
-                <div className='message' id={name === messageContent.author ? "you" : "other"}>
-                    <div>
-                        <div className='message-content'>
-                            <p>{messageContent.message}</p>
-                        </div>
-                        <div className='message-meta'>
-                            <p id="time">{messageContent.time}</p>
-                            {/* {console.log(messageContent.author)}
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent,index) => {
+            // return <h1>{messageContent.message}</h1>
+            return (
+              <div
+                className="message" key={index}
+                id={name === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    {/* {console.log(messageContent.author)}
                             {console.log(messageContent.time)} */}
-                            
-                            <p id="author">{messageContent.author}</p>
-                        </div>
-                    </div>
-                </div>
-                )
-            })}
-            </ScrollToBottom>
-        </div>
-        
-        {/* ):( */}
 
-        <div className='chat-footer'>
-        <input type="text" value={currentMessage} placeholder="Chat comes here..." onChange={(event)=>{
-            setCurrentMessage(event.target.value);
-        }} onKeyPress={(event)=>{
-            event.key ==="Enter" && sendMessgae();
-        }}/>
-        <button onClick={sendMessgae} style={myStyle}>&#9658;</button>
-        </div>
-        {/* )} */}
-        
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+
+      {/* ):( */}
+
+      <div className="chat-footer">
+        {/* <div>
+        </div> */}
+        {/* <div> */}
+          <input
+            type="text"
+            value={currentMessage}
+            placeholder="Chat comes here..."
+            onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && sendMessgae();
+            }}
+          />
+          {/* <div> */}
+            <button onClick={sendMessgae} style={myStyle}>
+              &#9658;
+            </button>
+          {/* </div>
+        </div> */}
+      </div>
+      {/* )} */}
     </div>
-  )
+  );
 }
